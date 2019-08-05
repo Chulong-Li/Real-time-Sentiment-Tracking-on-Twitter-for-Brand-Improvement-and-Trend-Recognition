37d942dec62f1d6d6d71d8e1241928d25aa0db1a
Chulong Li <li.7003@osu.edu>
2019-08-05 17:13:15 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you
+api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+api_secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"

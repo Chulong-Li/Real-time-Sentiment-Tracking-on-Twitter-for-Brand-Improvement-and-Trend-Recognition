9301c577c27bc5b5ce3714f2108db3e4de8edd28
Chulong Li <li.7003@osu.edu>
2019-08-06 15:55:42 -0400

    Update the ETL pipline

--- settings.py
+++ settings.py
@@ -1,2 +1,6 @@
 TRACK_WORDS = ['datascience', 'data science']
 TABLE_NAME = "data_science"
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at VARCHAR(255), text VARCHAR(255), \
+            polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
+            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
+            retweet_count INT, favorite_count INT"

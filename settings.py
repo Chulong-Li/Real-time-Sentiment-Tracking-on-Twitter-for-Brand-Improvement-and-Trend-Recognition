d602967fb3202858d47644f77ad9848e68517a1e
Chulong Li <li.7003@osu.edu>
2019-08-08 16:43:13 -0400

    add Dash plot based on time series

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-TRACK_WORDS = ['trump']
-TABLE_NAME = "Trump"
-TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at VARCHAR(255), text VARCHAR(255), \
+TRACK_WORDS = ['AI']
+TABLE_NAME = "AI"
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
             polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
             user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
             retweet_count INT, favorite_count INT"

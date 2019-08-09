d2448fa75439b1351de0c946ce1a085e506b6e7a
Chulong Li <li.7003@osu.edu>
2019-08-08 20:22:19 -0400

    Add topic trend analysis

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-TRACK_WORDS = ['AI']
-TABLE_NAME = "AI"
+TRACK_WORDS = ['Trump']
+TABLE_NAME = "Trump"
 TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
             polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
             user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \

0cea7c7e6968ca9e486d8112f40c1f03c2d79a9d
Chulong Li <li.7003@osu.edu>
2019-08-10 16:48:49 -0400

    Change settings

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-TRACK_WORDS = ['Trump']
-TABLE_NAME = "Trump"
+TRACK_WORDS = ['Facebook']
+TABLE_NAME = "Facebook"
 TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
             polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
             user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \

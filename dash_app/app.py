7c1757cc254d1ddbb7f0960135da939b6499d3a4
Chulong Li <li.7003@osu.edu>
2019-08-14 19:02:07 -0400

    Update app

--- dash_app/app.py
+++ dash_app/app.py
@@ -4,6 +4,16 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 import settings
+import itertools
+import math
+
+import re
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,10 +21,57 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('sample_data.csv')
 df['created_at'] = pd.to_datetime(df['created_at'])
- # Clean and transform data to enable time series
+
+# Clean and transform data to enable time series
 result = df.groupby([pd.Grouper(key='created_at', freq='2s'), 'polarity']).count().unstack(fill_value=0).stack().reset_index()
 result = result.rename(columns={"id_str": "Num of '{}' mentions".format(settings.TRACK_WORDS[0]), "created_at":"Time in UTC"})  
 time_series = result["Time in UTC"][result['polarity']==0].reset_index(drop=True)
+
+# Clean and transform data to enable word frequency
+content = ' '.join(df["text"])
+content = re.sub(r"http\S+", "", content)
+content = content.replace('RT ', ' ').replace('&amp;', 'and')
+content = re.sub('[^A-Za-z0-9]+', ' ', content)
+content = content.lower()
+
+# Filter constants for states in US
+STATES = ['Alabama', 'AL', 'Alaska', 'AK', 'American Samoa', 'AS', 'Arizona', 'AZ', 'Arkansas', 'AR', 'California', 'CA', 'Colorado', 'CO', 'Connecticut', 'CT', 'Delaware', 'DE', 'District of Columbia', 'DC', 'Federated States of Micronesia', 'FM', 'Florida', 'FL', 'Georgia', 'GA', 'Guam', 'GU', 'Hawaii', 'HI', 'Idaho', 'ID', 'Illinois', 'IL', 'Indiana', 'IN', 'Iowa', 'IA', 'Kansas', 'KS', 'Kentucky', 'KY', 'Louisiana', 'LA', 'Maine', 'ME', 'Marshall Islands', 'MH', 'Maryland', 'MD', 'Massachusetts', 'MA', 'Michigan', 'MI', 'Minnesota', 'MN', 'Mississippi', 'MS', 'Missouri', 'MO', 'Montana', 'MT', 'Nebraska', 'NE', 'Nevada', 'NV', 'New Hampshire', 'NH', 'New Jersey', 'NJ', 'New Mexico', 'NM', 'New York', 'NY', 'North Carolina', 'NC', 'North Dakota', 'ND', 'Northern Mariana Islands', 'MP', 'Ohio', 'OH', 'Oklahoma', 'OK', 'Oregon', 'OR', 'Palau', 'PW', 'Pennsylvania', 'PA', 'Puerto Rico', 'PR', 'Rhode Island', 'RI', 'South Carolina', 'SC', 'South Dakota', 'SD', 'Tennessee', 'TN', 'Texas', 'TX', 'Utah', 'UT', 'Vermont', 'VT', 'Virgin Islands', 'VI', 'Virginia', 'VA', 'Washington', 'WA', 'West Virginia', 'WV', 'Wisconsin', 'WI', 'Wyoming', 'WY']
+STATE_DICT = dict(itertools.zip_longest(*[iter(STATES)] * 2, fillvalue=""))
+INV_STATE_DICT = dict((v,k) for k,v in STATE_DICT.items())
+
+# Clean and transform data to enable geo-distribution
+is_in_US=[]
+geo = df[['user_location']]
+df = df.fillna(" ")
+for x in df['user_location']:
+    check = False
+    for s in STATES:
+        if s in x:
+            is_in_US.append(STATE_DICT[s] if s in STATE_DICT else s)
+            check = True
+            break
+    if not check:
+        is_in_US.append(None)
+
+geo_dist = pd.DataFrame(is_in_US, columns=['State']).dropna().reset_index()
+geo_dist = geo_dist.groupby('State').count().rename(columns={"index": "Number"}) \
+        .sort_values(by=['Number'], ascending=False).reset_index()
+geo_dist["Log Num"] = geo_dist["Number"].apply(lambda x: math.log(x, 2))
+
+
+geo_dist['Full State Name'] = geo_dist['State'].apply(lambda x: INV_STATE_DICT[x])
+geo_dist['text'] = geo_dist['Full State Name'] + '<br>' + 'Num: ' + geo_dist['Number'].astype(str)
+
+
+tokenized_word = word_tokenize(content)
+stop_words=set(stopwords.words("english"))
+filtered_sent=[]
+for w in tokenized_word:
+    if w not in stop_words:
+        filtered_sent.append(w)
+fdist = FreqDist(filtered_sent)
+fd = pd.DataFrame(fdist.most_common(30), columns = ["Word","Frequency"]).drop([0]).reindex()
+
 
 server = app.server
 
@@ -55,41 +112,60 @@ app.layout = html.Div([
     }),
 
     html.Div([
-        html.Div([
-            dcc.Graph(
-                id='crossfilter-indicator-scatter',
-                figure={
-                    'data': [
-                        go.Scatter(
-                            x=time_series,
-                            y=result["Num of '{}' mentions".format(settings.TRACK_WORDS[0])][result['polarity']==0].reset_index(drop=True),
-                            name="Neural",
-                            opacity=0.8
-                        ),
-                        go.Scatter(
-                            x=time_series,
-                            y=result["Num of '{}' mentions".format(settings.TRACK_WORDS[0])][result['polarity']==-1].reset_index(drop=True),
-                            name="Negative",
-                            opacity=0.8
-                        ),
-                        go.Scatter(
-                            x=time_series,
-                            y=result["Num of '{}' mentions".format(settings.TRACK_WORDS[0])][result['polarity']==1].reset_index(drop=True),
-                            name="Positive",
-                            opacity=0.8
-                        )
-                    ]
-                }
-            )
-
-        ]),
-
-        html.Div([
-            dcc.Graph(id='x-time-series'),
-            dcc.Graph(id='y-time-series'),
-        ], style={'display': 'inline-block', 'width': '49%'})
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=time_series,
+                        y=result["Num of '{}' mentions".format(settings.TRACK_WORDS[0])][result['polarity']==0].reset_index(drop=True),
+                        name="Neural",
+                        opacity=0.8
+                    ),
+                    go.Scatter(
+                        x=time_series,
+                        y=result["Num of '{}' mentions".format(settings.TRACK_WORDS[0])][result['polarity']==-1].reset_index(drop=True),
+                        name="Negative",
+                        opacity=0.8
+                    ),
+                    go.Scatter(
+                        x=time_series,
+                        y=result["Num of '{}' mentions".format(settings.TRACK_WORDS[0])][result['polarity']==1].reset_index(drop=True),
+                        name="Positive",
+                        opacity=0.8
+                    )
+                ]
+            }
+        )
     ]),
-        
+
+    html.Div([
+        dcc.Graph(
+            id='x-time-series',
+            figure = {
+                'data':[
+                    go.Bar(x=fd["Word"], y=fd["Frequency"], name="Freq Dist")
+                ]
+            }        
+        ),
+        dcc.Graph(
+            id='y-time-series',
+            figure = {
+                'data':[
+                    go.Choropleth(
+                        locations=geo_dist['State'], # Spatial coordinates
+                        z = geo_dist['Log Num'].astype(float), # Data to be color-coded
+                        locationmode = 'USA-states', # set of locations match entries in `locations`
+                        colorscale = "Blues",
+                        text=geo_dist['text'], # hover text
+                        showscale=False,
+                        geo = 'geo'
+                    )
+                ]
+            }
+        ),
+    ], style={'display': 'inline-block', 'width': '49%', 'columnCount': 2}),
+
     html.Div([
         html.Label('Slider'),
         dcc.Slider(
@@ -98,9 +174,10 @@ app.layout = html.Div([
             marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
             value=5
         )
-    ])
+    ])    
+
 ])
-#, style={'columnCount': 1}
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
